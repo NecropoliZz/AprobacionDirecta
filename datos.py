2420b6662c231e08787d1bd447a77c8efbc18045
David Soria <necropoliz7878@gmail.com>
2025-07-01 10:56:14 -0300

    Add files via upload

---
+++ datos.py
@@ -0,0 +1,11 @@
+estudiantes = ["Ana","Bruno","Carla","Diego"]
+calificaciones = [
+    [9,8,10],
+    [6,7,8],
+    [10,10,9],
+    [7,6,5]
+]
+materias = ["matematica","historia","biologia"]
+running = True
+limite_inferior = 1
+limite_superior = 5
